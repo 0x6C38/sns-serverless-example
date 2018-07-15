5a554237a5c3f8082280c1b89d837833297c25d0
Danilo Salvio <danilo_salvio@live.com>
2018-07-15 20:32:13 -0300

    Adds environment variable with Topic Arn.

--- handler.js
+++ handler.js
@@ -1,16 +1,15 @@
 'use strict';
 
+const TOPIC_URL = process.env.TOPIC_ARN;
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: TOPIC_URL
     }),
   };
 
   callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
