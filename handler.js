9d1ea84cfb45bedfd054328e0bb89fbd199548fc
Danilo Salvio <danilo_salvio@live.com>
2018-07-15 21:04:50 -0300

    Adds IAM role to publish to SNS.

--- handler.js
+++ handler.js
@@ -1,12 +1,27 @@
 'use strict';
 
 const TOPIC_URL = process.env.TOPIC_ARN;
+const AWS = require('aws-sdk');
 
-module.exports.hello = (event, context, callback) => {
+module.exports.publishToSNS = (event, context, callback) => {
+  const message = (event.body === undefined ? 'No-message' : event.body); //JSON.parse(event.body) if message is JSON object
+  const sns = new AWS.SNS();
+
+  const params = {
+    Message: message,
+    TopicArn: TOPIC_ARN
+  };
+
+  try {
+    sns.publish(params).promise();
+  } catch(error) {
+    console.log(error);
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: TOPIC_URL
+      message: message
     }),
   };
 
